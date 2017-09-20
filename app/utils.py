0da24390e59f62aec9caf23b5558b95abeae28f4
Pius Dan Nyongesa <npiusdan@gmail.com>
2017-09-20 12:35:39 +0300

    fixing front-end added custom function to create users

--- app/utils.py
+++ app/utils.py
@@ -13,30 +13,20 @@ def flash_errors(form):
             ), category="errors")
 
 
-def create_user(self, username, phone_number, email, password, country, city):
+def create_user(username, phone_number, email, password, country, city):
     user = User(phone_number=phone_number,
                 password=password,
                 country=country,
-                city=city,
                 username=username,
                 email=email)
-    if city is not None:
-        try:
-            country = 
-    if address is not None:
-        location = Location.query.filter_by(address=address.capitalize()).first()
-        if location:
-            location = location
-        else:
-            try:
-                location = Location(address=address)
-            except GeocoderError as exc:
-                raise self.retry(exc=exc, countdown=5)
-    else:
-        codes = {"+254": "Kenya", "+255": "Uganda"}
-        location = Location(country=codes[phone_number[:4]])
+    try:
+        geocoder = googlev3.GoogleV3()
+        location = str(geocoder.geocode(city).address)
+        user.city, user.country = location.split(", ")
+    except Exception as exc:
+        user.city = city
+        user.country = None
 
     db.session.add(user)
     db.session.commit()
     return True
-
