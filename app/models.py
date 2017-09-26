4505d5fab02b0a135f37e67c8e76b53b2deab0eb
Pius Dan Nyongesa <npiusdan@gmail.com>
2017-09-27 00:36:03 +0300

    fixing deployment bugs

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import hashlib
-import cPickle as pickle
+import pickle
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import request
--- config.py
+++ config.py
@@ -6,7 +6,6 @@
 """
 
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
