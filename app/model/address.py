440eeb91266db0ae7066b95ecc6d06506d969f55
Pius Dan Nyongesa <npiusdan@gmail.com>
2017-11-07 00:08:22 +0300

    FIX added an id an primary key to  Address and Code

--- app/model/address.py
+++ app/model/address.py
@@ -11,6 +11,7 @@ class Address(db.Model):
     :param code: Association to Code
     """
     __tablename__ = "address"
+    id = Column(Integer, primary_key=True)
     city = Column(String(64))
     code_id = Column(Integer, ForeignKey('codes.id'))
     code = relationship('Code', back_populates="address", lazy="subquery")
@@ -22,6 +23,7 @@ class Code(db.Model):
     :param country_code: Telephone code for the country
     """
     __tablename__ = 'codes'
+    id = Column(Integer, primary_key=True)
     country = Column(String(12), Nullable=False, unique=True)
     currency_code = Column(String(4), nullable=False)
     country_code = Column(String(4), nullable=False)
