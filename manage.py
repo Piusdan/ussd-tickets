2d4c27a81fd2f2f4047817a4e34178853649e20c
Pius Dan Nyongesa <npiusdan@gmail.com>
2018-02-10 09:46:37 +0300

    More changes

--- manage.py
+++ manage.py
@@ -78,7 +78,6 @@ def superuser(username, phonenumber, password):
         return
     role = Role.get_admin()
     user = User.query.filter_by(phone_number=phonenumber).first() or User.query.filter_by(username=username).first()
-    user.role = role
     if user is not None:
         click.echo(click.style('phone number Already in Use', fg='red'))
         return
@@ -87,6 +86,7 @@ def superuser(username, phonenumber, password):
     code = Code.by_country("Uganda")
     add.code = code
     user.address = add
+    user.role = role
     # commit I say Commit this changes
     user.save()
     click.echo(click.style('Super User created', fg='green'))
