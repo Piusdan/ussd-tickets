8be1b1e980555cb98c9294170bf7faf60976d0ad
Pius Dan Nyongesa <npiusdan@gmail.com>
2018-02-10 10:53:10 +0300

    Big fix #1 Auth

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -3,23 +3,24 @@ from flask_httpauth import HTTPBasicAuth
 
 from . import api
 from .errors import unauthorised, forbidden
-from ..model import User
+from ..model import User, AnonymousUser
 
 api_auth = HTTPBasicAuth()
 
 
 @api_auth.verify_password
-def verify_password(phone_number_or_token, password):
-    if phone_number_or_token == "":
-        raise ValueError("Password or Token missing!")
-    if password == '':
-        g.current_user = User.verify_auth_token(phone_number_or_token)
-        if g.current_user is None: return forbidden("Invalid Auth Token")
-        g.token_used = True
+def verify_password(email_or_token, password):
+    if email_or_token == "":
+        if email_or_token == '':
+            g.current_user = AnonymousUser()
         return True
-    user = User.query.filter_by(phone_number=phone_number_or_token).first() or User.query.filter_by(email=phone_number_or_token).first()
+    if password == '':
+        g.current_user = User.verify_auth_token(email_or_token)
+        g.token_used = True
+        return g.current_user is not None
+    user = User.query.filter_by(email=email_or_token).first() or User.query.filter_by(phone_number=email_or_token).first()
     if not user:
-        raise Exception("Invalid User")
+        return False
     g.current_user = user
     g.token_used = False
     return user.verify_password(password)
